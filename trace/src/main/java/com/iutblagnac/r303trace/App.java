b89700e5c7f851217f61f15af5694474f8476b52
Vico <79311637+youluckk@users.noreply.github.com>
2025-09-18 11:14:01 +0200

    Initial commit

---
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -0,0 +1,21 @@
+package com.iutblagnac.r303trace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+
+    public static String hello() {
+        return "Hello World!";
+    }
+
+    public static String hello(String param) {
+        return param;
+    }
+
+}
